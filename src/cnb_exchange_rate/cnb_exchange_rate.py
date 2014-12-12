94f14f3f053adbafdcdd2e1f74ea54f8d3c8e620
Stepan Sojka <stepansojka@countermail.com>
2014-12-12 21:44:35 +1300

    refactoring, clean up

--- src/cnb_exchange_rate/cnb_exchange_rate.py
+++ src/cnb_exchange_rate/cnb_exchange_rate.py
@@ -19,6 +19,12 @@ def set_host(h):
     global host
     host = h
 
+def download(url):
+    stream = urlopen(url)
+    raw = stream.read()
+
+    return raw.decode('ascii', 'ignore')
+
 def parse_table(table):
     csv_reader = csv.reader(table.split('\n'), delimiter=FIELD_DELIMITER)
     d = {}
@@ -31,16 +37,11 @@ def parse_table(table):
 
 
 def download_average_rate_table(currency, table_index):
-    stream = urlopen(URL % (host, currency))
-    raw = stream.read()
-
-    if len(raw) < 16:
-        raise ValueError('currency %s not found' % currency)
-    
-    tables = raw.decode('ascii', 'ignore').split(TABLE_DELIMITER)
+    url = URL % (host, currency)
+    tables = download(url).split(TABLE_DELIMITER)
     return tables[table_index]
 
-def average_rates(currency, table_index):
+def average_rates_table(currency, table_index):
     raw_table = download_average_rate_table(currency, table_index)
     return parse_table(raw_table)
 
@@ -48,37 +49,33 @@ def rate_string_to_float(s):
     return float(s.replace(',','.'))
 
 def monthly_average(currency, year, month):
-    rate_table = average_rates(currency, MONTHLY_AVERAGE_TABLE_IDX)
-
     try:
+        rate_table = average_rates_table(currency, MONTHLY_AVERAGE_TABLE_IDX)
         return rate_string_to_float(rate_table[str(year)][month - 1])
     except (ValueError, KeyError, IndexError):
         raise ValueError('average rate for %s, year %s, month %s not found' % (currency, year, month))
 
 
 def cumulative_monthly_average(currency, year, month):
-    rate_table = average_rates(currency, CUMULATIVE_MONTHLY_AVERAGE_TABLE_IDX)
-
     try:
+        rate_table = average_rates_table(currency, CUMULATIVE_MONTHLY_AVERAGE_TABLE_IDX)
         return rate_string_to_float(rate_table[str(year)][month - 1])
     except (ValueError, KeyError, IndexError):
         raise ValueError('cumulative average rate for %s, year %s, month %s not found' % (currency, year, month))
 
 
 def quarterly_average(currency, year, quarter):
-    rate_table = average_rates(currency, QUARTERLY_AVERAGE_TABLE_IDX)
-
     try:
+        rate_table = average_rates_table(currency, QUARTERLY_AVERAGE_TABLE_IDX)
         return rate_string_to_float(rate_table[str(year)][quarter - 1]) 
     except (ValueError, KeyError, IndexError):
         raise ValueError('average rate for %s, year %s, quarter %s not found' % (currency, year, quarter))
 
 def daily_rate(currency, date):
     date_str = date.strftime('%d.%m.%Y')
-    stream = urlopen(DAILY_URL % (host, currency, date_str, date_str))
-    raw = stream.read()
+    url = DAILY_URL % (host, currency, date_str, date_str)
     
-    table = parse_table(raw.decode('ascii', 'ignore'))
+    table = parse_table(download(url))
 
     rate_str = table[date_str][0]
     return rate_string_to_float(rate_str)
